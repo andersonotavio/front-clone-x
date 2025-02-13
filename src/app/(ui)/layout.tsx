f76809b468a5a61cc4f203d997d429c714cd1982
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2025-02-13 20:54:33 -0300

    profile date created

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { NavItem } from "@/components/nav/nav-item";
+import { NavLogout } from "@/components/nav/nav-logout";
+import { NavMyProfile } from "@/components/nav/nav-profile";
 import { Logo } from "@/components/ui/logo";
 import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 
@@ -27,8 +29,9 @@ const Layout = ({children}: Props) => {
             />
           </nav>
         </div>
-        <div>
-          ...
+        <div className="mb-6 flex flex-col  gap-4">
+          <NavLogout />
+          <NavMyProfile />
         </div>
       </section>
       <section className="flex-1 max-w-lg">{children}</section>
