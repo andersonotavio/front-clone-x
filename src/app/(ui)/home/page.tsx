405a3fe1ccf12f6d4103a0286078808913c21033
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2025-02-17 15:56:00 -0300

    Post comments

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,10 +1,12 @@
 import { HomeHeader } from "@/components/home/home-header";
+import { TweePost } from "@/components/tweets/tweet-post";
 
 export default function Page() {
 
   return (
     <div>
       <HomeHeader />  
+      <TweePost />
     </div>
   );
 }
